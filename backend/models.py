08ec5bba4f5dc63b4856193338d156fd324e323d
Ernest Litvinenko <ernest.litvinenko@mail.ru>
2022-07-10 23:11:08 +1000

    Add models

--- backend/models.py
+++ backend/models.py
@@ -1,3 +1,21 @@
+from django.contrib.auth import get_user_model
+from django.conf import settings
+
 from django.db import models
 
-# Create your models here.
+Users: settings.AUTH_USER_MODEL = get_user_model()
+
+
+class Topics(models.Model):
+    owner = models.ForeignKey(Users, on_delete=models.CASCADE)
+    topic_name = models.CharField(max_length=100)
+
+
+class Messages(models.Model):
+    owner = models.ForeignKey(Users, on_delete=models.CASCADE)
+    message = models.CharField(max_length=300)
+
+
+class RepliesRelation(models.Model):
+    message = models.ForeignKey(Messages, on_delete=models.CASCADE)
+    reply = models.ForeignKey(Messages, on_delete=models.CASCADE)
